c0b41adcd819442eb928a61011e42139d4bd19f2
Apna-knowledge <162033046+Apna-knowledge@users.noreply.github.com>
2024-03-16 23:16:43 +0530

    syntax change

--- duble.cpp
+++ duble.cpp
@@ -1,11 +1,12 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-int main(){
-float f=23.23;
-double num;
-cout<<setprecision(3)<<f<<endl;
-double d=num/3;
-cout<<d<<endl;
-return 0;
+int main()
+{
+    float f = 2333.23;
+    double num;
+    cout << setprecision(3) << f << endl;
+    double d = num / 3;
+    cout << d << endl;
+    return 0;
 }
